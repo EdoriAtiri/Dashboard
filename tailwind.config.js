f27f84561df5d9ff19cd62bc12fb8a2567586eeb
Edori Atiri <59481349+EdoriAtiri@users.noreply.github.com>
2022-03-01 10:56:42 +0100

    added sidebar component

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        "ha-blue": "#397FFE",
+        "light-gray": "#9CA1AC",
+      },
+      fontSize: {
+        tiny: ".875rem",
+      },
+    },
+  },
+  plugins: [],
+};
